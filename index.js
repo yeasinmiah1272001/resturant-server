18b540083db31a73571bea657f1fd6ca1454e944
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-01-25 17:28:50 +0600

    admin role complete

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USSER}:${process.env.DB_PASS}@cluster0.qlvqjvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
